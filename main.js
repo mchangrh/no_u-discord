10f4636beb40bd6f8e4ccff31b72c7b06591085e
Michael <mchangrh@gmail.com>
2019-03-26 19:02:51 -0400

    added discord.js starter

---
+++ main.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login("token");
+
+client.on("ready", () => {
+    console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login('token');
