a769694319d8f6ef38adbfdcacb3f281ff150dd3
Michael <mchangrh@gmail.com>
2019-03-26 21:06:16 -0400

    fixed grr and marine

--- commands/grr.js
+++ commands/grr.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'alpha',
-	description: 'I Alpha',
+	name: 'grr',
+	description: 'jess-grr',
 	execute(message, args) {
 			message.channel.send({attachment: "../assets/jess-grr.mp3",
 				name:"grr.mp3"});
