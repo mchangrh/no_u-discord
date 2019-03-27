fcaaf47c111bd654a340c1e0ddad62d3985d987a
Jonathan Chung <jonathanchung0115@gmail.com>
2019-03-26 22:42:29 -0400

    added yaml support

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
 // imports
-const fs = require('fs');
 const Discord = require("discord.js");
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 // create client and collection
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-// import all files
+// generate commands from data files
+try {
+	const textCommands = yaml.safeLoad(fs.readFileSync('./data/text_commands.yml', 'utf8'));
+	textCommands.forEach(({ name, description, type, data, extraData }) => {
+		const command = { name, description };
+		if (type === 'text') {
+			command.execute = (messageService, args) => {
+				messageService.channel.send(data);
+			};
+		} else if (type === 'image') {
+			command.execute = (messageService, args) => {
+				messageService.channel.send({ file: data });
+			};
+		} else if (type === 'audio') {
+			command.execute = (messageService, args) => {
+				messageService.channel.send({ files: [{ attachment: data, name: extraData }] });
+			};
+		}
+		client.commands.set(name, command);
+	});
+} catch (err) {
+	console.log(err);
+}
+
+// import all command files
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-// import configs 
-const config = require("./config.json");
 
 client.on("ready", () => {
 	console.log('Ready');
 });
 
+// import configs 
+const config = require("./config.json");
 const prefix = config.prefix;
 client.on('message', message => {
 	// check for Prefix
