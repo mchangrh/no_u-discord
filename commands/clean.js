2be3381512302def0f0bee47cc93e230c5a0a5ad
Jonathan Chung <jonathanchung0115@gmail.com>
2019-03-27 23:30:20 -0400

    spaces -> tabs

--- commands/clean.js
+++ commands/clean.js
@@ -1,32 +1,33 @@
-const config = require("../config.json");
+module.exports = (message, args) => {
+	// start cleaning
+	message.channel.send('start clean')
+	// fetch messages
+	message.channel.fetchMessages()
+	// collect and filter through delOrNot
+	.then((messages) => {
+		return messages.filter((msg) => {
+			return delOrNot(msg);
+		});
+	})
+	.then((collect) => {
+		bulkDel(collect, message);
+	})
+	.catch(console.error);
 
-module.exports = {
-	name: 'clean',
-	description: 'cleans channel',
-	execute(message, args) {
-        // start cleaning
-        message.channel.send('start clean')
-        // fetch messages
-        const allMessages = message.channel.fetchMessages()
-            // collect and filter through delOrNot
-            .then(messages => { return messages.filter(msg => delOrNot(msg)) } )
-            .then(collect => bulkDel(collect, message))
-            .catch(console.error);
+	// wait to delete message
+	message.channel.fetchMessage(message.channel.lastMessageID)
+		.then(message => message.delete(5000))
+		.catch(console.error);
+}
 
-        function bulkDel(botMessages, message) {
-            // lol just delete
-            message.channel.bulkDelete(botMessages)
-                .then(messages => message.channel.send(`Bulk deleted ${messages.size} messages`))
-                .catch(console.error);
-        };
-        // wait to delete message
-        channel.fetchMessage(channel.lastMessageID)
-            .then(message => message.delete(5000))
-            .catch(console.error);
-    }
+function bulkDel(botMessages, message) {
+	// lol just delete
+	message.channel.bulkDelete(botMessages)
+		.then(messages => message.channel.send(`Bulk deleted ${messages.size} messages`))
+		.catch(console.error);
 };
 
-function chooseDel(msg) {
-    const isCommand = msg.content.startsWith("!") || msg.content.startsWith("~");
-    return msg.author.bot || isCommand;
+function delOrNot(msg) {
+	const isCommand = msg.content.startsWith("!") || msg.content.startsWith("~");
+	return msg.author.bot || isCommand;
 }
