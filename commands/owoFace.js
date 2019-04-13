05cde1162c2cd6865e41bb7c175cd0b500da620a
Michael <mchangrh@gmail.com>
2019-04-13 02:03:23 -0400

    @chyyran made factories

--- commands/owoFace.js
+++ commands/owoFace.js
@@ -2,17 +2,20 @@ const eyes = ['^', 'U', ';;', '♥', '✧', '☆', '@', '◕', '◔', 'ᅌ', '�
 const mouth = ['w', '𝓌', '𝔀', '𝕨', '𝐰', '𝙬', '𝔴', '𝖜', 'ω', 'ᗯ', 'ᵕ', '꒳', 'Ꮗ', '\u0077\u0332', '\u0077\ufe20']
 const enclosure = [['(', ')'], ['(。', '。)'], ['ღ(', 'ღ)'], ['', '']]
 
-const owoFactory = (eyes, mouth, enclosure) => () => {
-  const randElement = (array) => array[Math.floor(Math.random() * array.length)]
+const randElement = (array) => array[Math.floor(Math.random() * array.length)]
+const randomFactory = (elements) => () => randElement(elements)
 
-  const randEyes = randElement(eyes)
-  const randMouth = randElement(mouth)
-  const randEnclosure = randElement(enclosure)
+const randEyeFactory = randomFactory(eyes)
+const randMouthFactory = randomFactory(mouth)
+const randEnclosureFactory = randomFactory(enclosure)
 
-  // enclosure, eye, mouth, eye, enclosure
-  return `${randEnclosure[0]}${randEyes}${randMouth}${randEyes}${randEnclosure[1]}`
+const owoFactory = (eyesFactory, mouthFactory, enclosureFactory) => () => {
+  let eye = eyesFactory()
+  let mouth = mouthFactory()
+  let enclosure = enclosureFactory()
+  return `${enclosure[0]}${eye}${mouth}${eye}${enclosure[1]}`
 }
 
 module.exports = (message, args, flags, config) => {
-  return owoFactory(eyes, mouth, enclosure)
+  return owoFactory(randEyeFactory, randMouthFactory, randEnclosureFactory)
 }
