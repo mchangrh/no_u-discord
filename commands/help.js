35e4d44d81cfa30f2b79eaaf7e4ec4592be5f65f
Jonathan Chung <jonathanchung0115@gmail.com>
2019-04-12 22:08:26 -0400

    added pagination to the help command

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,29 @@
-module.exports = (message, args, flags, { prefix, commands }) => {
-	const helpMessage = commands.array().reduce((helpMessage, { name, description }) => {
-		return helpMessage.concat(`${prefix}${name}: ${description}\n`);
-	}, '');
+module.exports = (message, args, flags, config) => {
+	// Validate arguments
+	if (args.length > 1) {
+		throw new Error(`Expecting 0-1 arguments; received ${args.length}`);
+	} else if (args.length === 1) {
+		if (isNaN(args[0])) {
+			throw new Error(`Expecting numeric argument; received ${args[0]}`);
+		}
+		
+		if (parseFloat(args[0]) % 1 !== 0) {
+			throw new Error(`Expecting integer argument; received ${args[0]}`);
+		}
+	}
+
+	const { commands, helpCommandsPerPage, prefix } = config;
+	const commandArray = commands.array();
+	const totalPages = Math.floor((commandArray.length - 1) / helpCommandsPerPage) + 1;
+	const argPage = args.length ? parseInt(args[0]) : 1;
+	const startPage = Math.min(Math.max(argPage, 1), totalPages);
+	const startIndex = (startPage - 1) * helpCommandsPerPage;
+
+	let helpMessage = `Page ${startPage}/${totalPages}:\n`;
+	for (let i = startIndex; i < Math.min(startIndex + helpCommandsPerPage, commandArray.length); ++i) {
+		const { name, description } = commandArray[i];
+		helpMessage += `${prefix}${name}: ${description}\n`;
+	}
 
 	return message.channel.send(helpMessage);
 }
