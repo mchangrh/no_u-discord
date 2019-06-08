f0acbca9f33fd8c2dca1217e2631434ba5eb0a93
Michael <mchangrh@gmail.com>
2019-06-08 19:09:47 -0400

    added wipe and linted clean

--- commands/wipe.js
+++ commands/wipe.js
@@ -1,26 +1,26 @@
 'use strict'
 
-const { DEFAULT_SCHEMAS, validate } = require('./../validation.js')
+const { DEFAULT_SCHEMAS, validate } = require('../validation.js')
 
 module.exports = async (message, args, flags, config) => {
   validate(args, DEFAULT_SCHEMAS.emptyArray)
   validate(flags, DEFAULT_SCHEMAS.emptyObject)
 
   // start cleaning
-  message.channel.send('start clean')
+  message.channel.send('deleting your messages')
   try {
     // fetch messages
     const messages = await message.channel.fetchMessages()
 
     // filter messages
     const botRelatedMessages = messages.filter((msg) => {
-      const isCommand = msg.content.startsWith('!') || msg.content.startsWith(config.prefix)
-      return msg.author.bot || isCommand
+      // Delete all messages by author
+      return msg.author === message.author
     })
 
     // delete messages
     const deletedMessages = await message.channel.bulkDelete(botRelatedMessages)
-    const toDelete = await message.channel.send(`Bulk deleted ${deletedMessages.size} messages`)
+    const toDelete = await message.channel.send(`Deleted ${deletedMessages.size} shameful messages`)
     return toDelete.delete(config.autoDeleteDelay)
   } catch (err) {
     console.error(err)
