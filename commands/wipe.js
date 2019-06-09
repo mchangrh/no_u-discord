964fac557da3a4304a0f99573fcf3e75b2c7084c
Michael <mchangrh@gmail.com>
2019-06-08 20:11:50 -0400

    added verification for wiping

--- commands/wipe.js
+++ commands/wipe.js
@@ -1,28 +1,51 @@
 'use strict'
 
-const { DEFAULT_SCHEMAS, validate } = require('../validation.js')
+const {
+  DEFAULT_SCHEMAS,
+  validate
+} = require('../validation.js')
+const reallyLongPhrase = '`Hi Yes Im Sure I Want To Do This I Understand The Consequences Do It Now`'
+const secretPhrase = 'ILoveL85'
 
 module.exports = async (message, args, flags, config) => {
-  validate(args, DEFAULT_SCHEMAS.emptyArray)
+  const argSchema = {
+    type: 'string',
+    required: true,
+    minLength: 2,
+    maxLength: 2
+  }
+
+  validate(args, argSchema)
   validate(flags, DEFAULT_SCHEMAS.emptyObject)
 
-  // start cleaning
-  message.channel.send('deleting your messages')
-  try {
-    // fetch messages
-    const messages = await message.channel.fetchMessages()
+  // check if phrase is correct and secret phrase is correct
+  if (args && args[0] === reallyLongPhrase.replace(/ /g, '').replace(/`/g, '') && args[1] === secretPhrase) {
+    // start cleaning
+    const startmsg = await message.channel.send('deleting your messages')
+    try {
+      // fetch messages
+      const messages = await message.channel.fetchMessages()
 
-    // filter messages
-    const botRelatedMessages = messages.filter((msg) => {
-      // Delete all messages by author
-      return msg.author === message.author
-    })
+      // filter messages
+      const botRelatedMessages = messages.filter((msg) => {
+        // Delete all messages by author
+        return msg.author === message.author
+      })
 
-    // delete messages
-    const deletedMessages = await message.channel.bulkDelete(botRelatedMessages, true)
-    const toDelete = await message.channel.send(`Deleted ${deletedMessages.size} shameful messages`)
-    return toDelete.delete(config.autoDeleteDelay)
-  } catch (err) {
-    console.error(err)
+      // delete messages
+      const deletedMessages = await message.channel.bulkDelete(botRelatedMessages, true)
+      const toDelete = await message.channel.send(`Deleted ${deletedMessages.size} shameful messages`)
+      startmsg.delete(config.autoDeleteDelay)
+      return toDelete.delete(config.autoDeleteDelay)
+    } catch (err) {
+      console.error(err)
+    }
+  } else {
+    const msg1 = await message.channel.send('this command wipes your messages. If you\'re sure you want to do this:')
+    const msg2 = await message.channel.send('resend with this phrase: ' + reallyLongPhrase + ' but without spaces')
+    const msg3 = await message.channel.send('followed by the secret code from the admins')
+    msg1.delete(config.autoDeleteDelay * 5)
+    msg2.delete(config.autoDeleteDelay * 5)
+    msg3.delete(config.autoDeleteDelay * 5)
   }
 }
